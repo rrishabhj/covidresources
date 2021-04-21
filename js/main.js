aff8a8a9383f5dd5da2ac144929c81662a7c5a49
mmt8640 <mohit.lamba@go-mmt.com>
2021-04-21 23:53:57 +0530

    add func

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-$(document).ready(function () {
-    
-    
-});
+function url_generator(city="delhi", args = []) {
+    added_args = ["verified", "covid"];
+    added_args.push(city);
+    url = "https://twitter.com/search?q="+added_args.join("+")+"("+args.join("+OR+")+")";
+    return url
+}
+
+console.log(url_generator(city="pune", args=["beds", "oxygen"]))
